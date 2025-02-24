2bfe1231b2260265f1ee06caeca8db299346abd1
Victor Oldensand <victormag24@gmail.com>
2025-02-24 12:41:21 +0100

    feat: add reachy vision file

---
+++ vision-reachy/reachy_vision.py
@@ -0,0 +1,14 @@
+"Cette fiche permet de regarder via le caméra de Reachy"
+from reachy_sdk import ReachySDK
+import cv2 as cv
+
+# Replace with the actual IP you've found.
+reachy = ReachySDK(host="138.195.196.90")
+
+while True:
+    # This let you access the last frame grabbed by Reachy left eye
+    # It's always the most up-to-date image
+    left_image = reachy.left_camera.last_frame
+
+    cv.imshow("left image", left_image)
+    cv.waitKey(30)
